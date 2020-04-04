2aae9a91fc4e5cef977128f9ab8dfafaf7544008
Arbo Lindma <arbo.lindma@gmail.com>
2020-04-04 22:12:07 +0300

    added UserMixn

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 import datetime
 
+from flask_login.mixins import UserMixin
 from peewee import *
 
 DATABASE = SqliteDatabase('social.db')
 
 
-class User(Model):
+class User(UserMixin, Model):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField(max_length=100)
