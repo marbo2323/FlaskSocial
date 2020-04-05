69ae02a23cceeb1d548da003ad6c1eb57de066fd
Arbo Lindma <arbo.lindma@gmail.com>
2020-04-05 15:20:11 +0300

    Added user manager and admin user

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, g
+from flask_login import LoginManager
 import models
 
 DEBUG = True
@@ -6,6 +7,19 @@ PORT = 8000
 HOST = '0.0.0.0'
 
 app = Flask(__name__)
+app.secret_key = "23wer234.ewfewq4554tr.32534rfew!f34e4543"
+
+login_namager = LoginManager()
+login_namager.init_app(app)
+login_namager.login_view = 'login'
+
+
+@login_namager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 
 @app.before_request
@@ -23,4 +37,11 @@ def after_request(response):
 
 
 if __name__ == '__main__':
+    models.initialize()
+    models.User.create(
+        username="admin",
+        email="admin@snw.com",
+        password="admin.123",
+        is_admin=True
+    )
     app.run(debug=DEBUG, host=HOST, port=PORT)
