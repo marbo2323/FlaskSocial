4c09129079741be34acec01cdeb12f7a14c07454
Arbo Lindma <arbo.lindma@gmail.com>
2020-04-05 18:37:06 +0300

    Added register form validator and route

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, g
+from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask_login import LoginManager
+
+import forms
 import models
 
 DEBUG = True
@@ -34,6 +36,25 @@ def after_request(response):
     """Close the database connection after each request."""
     g.db.close()
     return response
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash("Yay, you registered!", "success")
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/')
+def index():
+    return 'Hey!'
 
 
 if __name__ == '__main__':
