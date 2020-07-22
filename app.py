1254681afea8fd72d36cd5582df621722d016a30
Arbo Lindma <arbo.lindma@gmail.com>
2020-07-22 19:58:32 +0300

    Added logout view

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for)
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user, login_required
 from flask_bcrypt import check_password_hash
 
 import forms
@@ -69,6 +69,14 @@ def login():
             else:
                 flash("Your email or password doesn't match!", "error")
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You've been logge out! Come back soon!", "success")
+    return redirect(url_for('index'))
 
 
 @app.route('/')
